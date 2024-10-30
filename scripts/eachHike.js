f5f8a9f6aa2cb5fb54902fc47e18610bf7768209
Kai Steingarten <ksteingarten@my.bcit.ca>
2024-10-29 21:44:29 -0700

    Add eachHike page and js

---
+++ scripts/eachHike.js
@@ -0,0 +1,20 @@
+function displayHikeInfo() {
+    let params = new URL(window.location.href); //get URL of search bar
+    let ID = params.searchParams.get("docID"); //get value for key "id"
+    console.log(ID);
+
+    db.collection("hikes")
+        .doc(ID)
+        .get()
+        .then((doc) => {
+            thisHike = doc.data();
+            hikeCode = thisHike.code;
+            hikeName = doc.data().name;
+
+            // only populate title, and image
+            document.getElementById("hikeName").innerHTML = hikeName;
+            let imgEvent = document.querySelector(".hike-img");
+            imgEvent.src = "../images/" + hikeCode + ".jpg";
+        });
+}
+displayHikeInfo();
